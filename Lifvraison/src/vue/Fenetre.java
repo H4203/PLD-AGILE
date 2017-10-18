d96d4cdb6b636e5f14ec7d5bac39a9c2bd2fa869
shikshak <ft.mezidi@gmail.com>
2017-10-18 08:13:01 +0200

    beau affichage avec plage horaire

--- Lifvraison/src/vue/Fenetre.java
+++ Lifvraison/src/vue/Fenetre.java
@@ -3,13 +3,19 @@ package vue;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -28,8 +34,10 @@ public class Fenetre extends JFrame
 	private VueGraphique vueGraphique;
 	private JButton jButtonChargement;
 	private JButton jButtonValider;
+	private JButton jButtonCalculTournee;
 	private JPanel jPanelBienvenue;
 	private EcouteurDeBoutons ecouteurDeBoutons;
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	
 	public Fenetre (Controleur controleur)
 	{
@@ -38,8 +46,10 @@ public class Fenetre extends JFrame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		jPanelBienvenue = new JPanel();
-		JLabel labelBienvenue = new JLabel("bienvenue");
-		jPanelBienvenue.add(labelBienvenue);
+		jPanelBienvenue.setBounds(400, screenSize.height-190, 500, 500);
+		JLabel jLabelBienvenue = new JLabel("~LIfvraison~");
+		jLabelBienvenue.setFont(new Font("Serif", Font.PLAIN, 30));
+		jPanelBienvenue.add(jLabelBienvenue);
 /*
 		setUndecorated(true);
         GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
@@ -74,12 +84,18 @@ public class Fenetre extends JFrame
 	{
 		getContentPane().removeAll();
 		//getContentPane().setLayout((LayoutManager) new FlowLayout(FlowLayout.RIGHT));
-		jButtonChargement = new JButton ( "Chargement Plan" );
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		jButtonChargement = new JButton ( "Charger Plan" );
 		jButtonChargement.setBounds(screenSize.width-200,screenSize.height-200,150,50);
 		//getContentPane().add( jButtonChargement, BorderLayout.SOUTH );
 		getContentPane().add( jButtonChargement );
 		getContentPane().add( jPanelBienvenue );
+		
+		JPanel imagePanel = new JPanel();
+		JLabel imageLabel = new JLabel(new ImageIcon("ihm\\image_livreur.jpg"));
+		imagePanel.setBounds(0,0,screenSize.width,screenSize.height);
+		imagePanel.add(imageLabel);
+		getContentPane().add(imagePanel);
+		
 		jButtonChargement.addActionListener( ecouteurDeBoutons );
 		
 		setVisible(true);
@@ -92,8 +108,7 @@ public class Fenetre extends JFrame
 		getContentPane().removeAll();
 		MapPanel mapPanel = new MapPanel(plan, null, null);
 		getContentPane().add(mapPanel);
-		jButtonChargement = new JButton ( "Chargement Livraison" );
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		jButtonChargement = new JButton ( "Charger Livraison" );
 		jButtonChargement.setBounds(screenSize.width-200,screenSize.height-200,150,50);
 		this.add( jButtonChargement );
 		
@@ -102,6 +117,24 @@ public class Fenetre extends JFrame
 		setVisible(true);
 	}
 
+	public void setModeDemandeLivraison (Plan plan, DemandeLivraison demandeLivraisons)
+	{
+		setVisible(false);
+		getContentPane().removeAll();
+
+		MapPanel mapPanel = new MapPanel(plan, demandeLivraisons);
+		getContentPane().add(mapPanel);
+		
+		jButtonCalculTournee = new JButton ( "Calculer Tournee" );
+		jButtonCalculTournee.setBounds(screenSize.width-200,screenSize.height-200,150,50);
+
+		getContentPane().add( jButtonCalculTournee);
+		getContentPane().add( jPanelBienvenue );
+		jButtonCalculTournee.addActionListener( ecouteurDeBoutons );
+		
+		setVisible(true);
+	}
+	
 	public void setModeTournee (Plan plan, DemandeLivraison demandeLivraisons, Tournee tournee)
 	{
 		setVisible(false);
@@ -111,10 +144,29 @@ public class Fenetre extends JFrame
 		getContentPane().add(mapPanel);
 		
 		jButtonValider = new JButton ( "Valider Tournee" );
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		jButtonValider.setBounds(screenSize.width-200,screenSize.height-200,150,50);
 
 		getContentPane().add( jButtonValider);
+		getContentPane().add( jPanelBienvenue );
+		jButtonValider.addActionListener( ecouteurDeBoutons );
+		
+		setVisible(true);
+	}
+	
+	public void setModeValiderTournee (Plan plan, DemandeLivraison demandeLivraisons, Tournee tournee)
+	{
+		setVisible(false);
+		getContentPane().removeAll();
+
+		MapPanel mapPanel = new MapPanel(plan, demandeLivraisons, tournee);
+		getContentPane().add(mapPanel);
+		
+		JPanel jPanelDeLaFin = new JPanel ();
+		JLabel jLabelBonneChance = new JLabel("Bonne Tournee ( ･ω･)ﾉ!!");
+		jPanelDeLaFin.add(jLabelBonneChance);
+		jPanelDeLaFin.setBounds(screenSize.width-200,screenSize.height-200,150,50);
+
+		getContentPane().add( jPanelDeLaFin);
 		getContentPane().add( jPanelBienvenue );
 		jButtonValider.addActionListener( ecouteurDeBoutons );
 		
--- Lifvraison/src/vue/MapPanel.java
+++ Lifvraison/src/vue/MapPanel.java
@@ -3,6 +3,7 @@ package vue;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
@@ -22,6 +23,7 @@ import modeles.DemandeLivraison;
 import modeles.Intersection;
 import modeles.Itineraire;
 import modeles.Livraison;
+import modeles.PlageHoraire;
 
 public class MapPanel extends JPanel 
 {
@@ -43,6 +45,42 @@ public class MapPanel extends JPanel
 		this.plan = plan;
 		this.demandeLivraison = demandeLivraison;
 		this.tournee = tournee;
+		
+		int xMax = 0;
+		int yMax = 0;
+		
+		for (Map.Entry<Long, Intersection> mapentry : plan.getListeIntersection().entrySet()) 
+        {
+			if (((Intersection) mapentry.getValue()).getX() > xMax)
+			{
+				xMax = ((Intersection) mapentry.getValue()).getX();
+			}
+			else if (((Intersection) mapentry.getValue()).getX() < xMin)
+			{
+				xMin = ((Intersection) mapentry.getValue()).getX();
+			}
+			
+			if (((Intersection) mapentry.getValue()).getY() > yMax)
+			{
+				yMax = ((Intersection) mapentry.getValue()).getY();
+			}
+			else if (((Intersection) mapentry.getValue()).getY() < yMin)
+			{
+				yMin = ((Intersection) mapentry.getValue()).getY();
+			}
+        }
+		
+		coefX = (double)(screenSize.height-bord) / (xMax - xMin);
+		coefY = (double)(screenSize.width-bord) / (yMax - yMin);
+	}
+	
+	public MapPanel(Plan plan, DemandeLivraison demandeLivraison)
+	{
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setBounds(10, 10, screenSize.width-bord, screenSize.height-bord);
+		
+		this.plan = plan;
+		this.demandeLivraison = demandeLivraison;
 		
 		int xMax = 0;
 		int yMax = 0;
@@ -105,39 +143,104 @@ public class MapPanel extends JPanel
         // gestion immonde mais temporaire
         if ( demandeLivraison != null)
         {
-	        for (Itineraire itineraire : tournee.getListeItineraires())
-	        {
-	        	for (Troncon troncon : itineraire.getTroncons())
-	        	{
-	        		g2.drawLine((int)Math.round((troncon.getIntersectionDepart().getY() - yMin) * coefY),
-	        				(int)(screenSize.height-bord) - (int)Math.round((troncon.getIntersectionDepart().getX() - xMin) * coefX),
-	            			(int)Math.round((troncon.getIntersectionArrive().getY() - yMin) * coefY),
-	            			(int)(screenSize.height-bord) - (int)Math.round((troncon.getIntersectionArrive().getX() - xMin) * coefX));
-	        	}
-	        }
-	        
+        	/* affiche la tournée */
+        	if ( tournee != null ) {
+        		int i = 0;
+        		for (Itineraire itineraire : tournee.getListeItineraires())
+    	        {
+    	        	for (Troncon troncon : itineraire.getTroncons())
+    	        	{
+    	        		g2.drawLine((int)Math.round((troncon.getIntersectionDepart().getY() - yMin) * coefY),
+    	        				(int)(screenSize.height-bord) - (int)Math.round((troncon.getIntersectionDepart().getX() - xMin) * coefX),
+    	            			(int)Math.round((troncon.getIntersectionArrive().getY() - yMin) * coefY),
+    	            			(int)(screenSize.height-bord) - (int)Math.round((troncon.getIntersectionArrive().getX() - xMin) * coefX));
+    	        	}
+    	        	
+    	        	g2.setFont(new Font("default", Font.BOLD, 16));
+    	        	i++;
+    	        	if ( i < tournee.getListeItineraires().size())
+    	        	{
+		        		/* horaires */
+						g2.drawString(tournee.getListeHoraire().get(i).getHeureDebut().toString()
+								+ " - " +
+								tournee.getListeHoraire().get(i).getHeureFin().toString(),
+								(int)Math.round((itineraire.getArrivee().getY() - yMin) * coefY),
+		            			(int)(screenSize.height-bord) - (int)Math.round((itineraire.getArrivee().getX() - xMin) * coefX));
+    	        	}
+    	        }
+        	}
+        	
+        	/* affiche les points */
+        	/* entrepot */
 	        g2.setColor(new Color(0, 150, 0));
 	        
 			g2.fillRect((int)Math.round((demandeLivraison.getEntrepot().getY() - yMin) * coefY) - 5, 
 					(int)(screenSize.height-bord) - (int)Math.round((demandeLivraison.getEntrepot().getX() - xMin) * coefX) - 5, 
 	    			10, 10);
 			
+			/*
+			BufferedImage img = null;
+			try {
+				img = ImageIO.read(new File("\\ihm\\mapmarker2.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        	g2.drawImage(img, (int)Math.round((demandeLivraison.getEntrepot().getY() - yMin) * coefY) - -10, 
+					(int)(screenSize.height-bord) - (int)Math.round((demandeLivraison.getEntrepot().getX() - xMin) * coefX) - -10, 
+        			20, 20, this);
+			 */
+			
+        	/* heure debut tournee - heure fin tournee */
+			g2.setFont(new Font("default", Font.BOLD, 16));
+			g2.setColor(Color.BLUE);
+			if (tournee != null)
+			{
+				g2.drawString(tournee.getListeHoraire().get(0).getHeureDebut().toString() + " - " + tournee.getListeHoraire().get(tournee.getListeHoraire().size()-1).getHeureDebut().toString(),
+						(int)Math.round((demandeLivraison.getEntrepot().getY() - yMin) * coefY) - 5, 
+						(int)(screenSize.height-bord) - (int)Math.round((demandeLivraison.getEntrepot().getX() - xMin) * coefX) - 5);
+			}
+			else
+			{
+				g2.drawString(demandeLivraison.getHeureDepart().toString(),
+						(int)Math.round((demandeLivraison.getEntrepot().getY() - yMin) * coefY) - 5, 
+						(int)(screenSize.height-bord) - (int)Math.round((demandeLivraison.getEntrepot().getX() - xMin) * coefX) - 5);
+			}
+			
+			/* points de livraisons */
 	        g2.setColor(Color.RED);
-	        
 	        for (Livraison livraison : demandeLivraison.getLivraisons()) 
 	        {
+	        	/* points de livraison */
 	        	g2.fillRect((int)Math.round((livraison.getIntersection().getY() - yMin) * coefY) - 4, 
 	        			(int)(screenSize.height-bord) - (int)Math.round((livraison.getIntersection().getX() - xMin) * coefX) - 4, 
 	        			8, 8);
-	        	
-	        	/*BufferedImage img = null;
+	        	/*
 				try {
-					img = ImageIO.read(new File("C:\\Users\\heyhey\\Desktop\\4IF\\AGILE\\IHM\\map-marker-icon2335464.png"));
+					img = ImageIO.read(new File("\\ihm\\mapmarker.png"));
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				}*/
-	        	//g.drawImage(img, 10, 10, this);
+				}
+	        	g2.drawImage(img, (int)Math.round((livraison.getIntersection().getY() - yMin) * coefY) -10,
+	        			(int)(screenSize.height-bord) - (int)Math.round((livraison.getIntersection().getX() - xMin) * coefX) -10,
+	        			20, 20, this);
+	        	*/
+	        	
+	        	/* heure depart et arrivee de la livraison */
+	        	g2.setFont(new Font("default", Font.BOLD, 16));
+				g2.setColor(Color.BLUE);
+				if (tournee == null)
+				{
+					PlageHoraire plhr = livraison.getPlagehoraire();
+					if ( plhr != null && plhr.getHeureDebut() != null && plhr.getHeureFin() != null )
+					{
+						g2.drawString(plhr.getHeureDebut() + " - " + plhr.getHeureFin(),
+										(int)Math.round((livraison.getIntersection().getY() - yMin) * coefY) - 4,
+										(int)(screenSize.height-bord) - (int)Math.round((livraison.getIntersection().getX() - xMin) * coefX) - 4);
+					}
+				}
+				g2.setColor(Color.RED);
 	        }
 	        
 	        
