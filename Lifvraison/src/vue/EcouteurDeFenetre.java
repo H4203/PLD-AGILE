80b555a24ec198fda495b6aababdec08d03a7cb3
Pache <etienne.raquin@insa-lyon.fr>
2017-11-08 17:36:49 +0100

    Format & Javadoc

--- Lifvraison/src/vue/EcouteurDeFenetre.java
+++ Lifvraison/src/vue/EcouteurDeFenetre.java
@@ -4,45 +4,38 @@ import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 
 /**
- * Classe EcouteurDeFenetre
- * Implemente ComponentListener
+ * Classe EcouteurDeFenetre Implemente ComponentListener
+ * 
  * @author H4203
  */
-public class EcouteurDeFenetre implements ComponentListener
-{
+public class EcouteurDeFenetre implements ComponentListener {
 	private VueGraphique vueGraphique;
 	private VueTextuelle vueTextuelle;
-	
-	public EcouteurDeFenetre(VueGraphique vueGraphique, VueTextuelle vueTextuelle) 
-	{
+
+	public EcouteurDeFenetre(VueGraphique vueGraphique, VueTextuelle vueTextuelle) {
 		this.vueGraphique = vueGraphique;
 		this.vueTextuelle = vueTextuelle;
 	}
 
 	/**
-	 * Gestion du redimensionnement
-	 * Appelle la mise a l'echelle de la VueGraphique
+	 * Gestion du redimensionnement Appelle la mise a l'echelle de la VueGraphique
 	 */
-	public void componentResized(ComponentEvent e) 
-    {
+	public void componentResized(ComponentEvent e) {
 		vueGraphique.resize();
-    }
-
-	@Override
-	public void componentHidden(ComponentEvent arg0) 
-	{
-		
 	}
 
 	@Override
-	public void componentMoved(ComponentEvent arg0) 
-	{
-		
+	public void componentHidden(ComponentEvent arg0) {
+
 	}
 
 	@Override
-	public void componentShown(ComponentEvent arg0) 
-	{
-		
+	public void componentMoved(ComponentEvent arg0) {
+
+	}
+
+	@Override
+	public void componentShown(ComponentEvent arg0) {
+
 	}
 }
--- Lifvraison/src/vue/EcouteurDeSouris.java
+++ Lifvraison/src/vue/EcouteurDeSouris.java
@@ -10,90 +10,82 @@ import java.awt.event.MouseWheelListener;
 import controleur.Controleur;
 
 /**
- * Classe EcouteurDeSouris
- * Implemente MouseListener, MouseMotionListener, et MouseWheelListener
+ * Classe EcouteurDeSouris Implemente MouseListener, MouseMotionListener, et
+ * MouseWheelListener
+ * 
  * @author H4203
  */
-public class EcouteurDeSouris implements MouseListener, MouseMotionListener, MouseWheelListener 
-{
+public class EcouteurDeSouris implements MouseListener, MouseMotionListener, MouseWheelListener {
 	/**
 	 * Point de debut et de fin pour la gestion du glisser deposer
 	 */
 	private Point startPoint;
 	private Point endPoint;
-	
+
 	private MapPanel mapPanel;
-	
+
 	private Controleur controleur;
-	
+
 	/**
-	 * Constructeur de EcouteurDeSouris
-	 * Initialise les points de debut et de fin
+	 * Constructeur de EcouteurDeSouris Initialise les points de debut et de fin
+	 * 
 	 * @param mapPanel
 	 * @param controleur
 	 */
-	public EcouteurDeSouris(MapPanel mapPanel, Controleur controleur)
-	{
+	public EcouteurDeSouris(MapPanel mapPanel, Controleur controleur) {
 		startPoint = new Point();
 		endPoint = new Point();
-		
+
 		this.mapPanel = mapPanel;
-		
+
 		this.controleur = controleur;
 	}
-	
+
 	/**
 	 * Gestion du glisser deposer
 	 */
 	@Override
-	public void mouseDragged(MouseEvent event) 
-	{
+	public void mouseDragged(MouseEvent event) {
 		endPoint = event.getPoint();
-		
+
 		controleur.mouseDrag(new Point(endPoint.x - startPoint.x, endPoint.y - startPoint.y));
-		
+
 		startPoint = endPoint;
 	}
 
 	@Override
-	public void mouseMoved(MouseEvent event) 
-	{
-		
+	public void mouseMoved(MouseEvent event) {
+
 	}
 
 	/**
 	 * Gestion du clic
 	 */
 	@Override
-	public void mouseClicked(MouseEvent event) 
-	{
+	public void mouseClicked(MouseEvent event) {
 		controleur.clicGauche(mapPanel.convertPoint(event.getPoint()));
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent event) 
-	{
-		
+	public void mouseEntered(MouseEvent event) {
+
 	}
 
 	@Override
-	public void mouseExited(MouseEvent event) 
-	{
-	
+	public void mouseExited(MouseEvent event) {
+
 	}
 
 	/**
 	 * Gestion de l'appui sur le clic
 	 */
 	@Override
-	public void mousePressed(MouseEvent event) 
-	{
+	public void mousePressed(MouseEvent event) {
 		startPoint = event.getPoint();
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent event) 
-	{
+	public void mouseReleased(MouseEvent event) {
 
 	}
 
@@ -101,8 +93,7 @@ public class EcouteurDeSouris implements MouseListener, MouseMotionListener, Mou
 	 * Gestion de la moette
 	 */
 	@Override
-	public void mouseWheelMoved(MouseWheelEvent event) 
-	{
+	public void mouseWheelMoved(MouseWheelEvent event) {
 		controleur.mouseWheel(event.getWheelRotation(), event.getPoint());
 	}
 }
