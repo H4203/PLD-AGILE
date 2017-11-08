926f04c47f3259b1305e13a26d7a4d2bd2e69250
shikshak <ft.mezidi@gmail.com>
2017-11-08 08:14:22 +0100

    qscfds

---
+++ Lifvraison/src/controleur/EtatIntervertirLivraisons1.java
@@ -0,0 +1,70 @@
+package controleur;
+
+import java.awt.Point;
+import java.util.List;
+
+import modeles.Intersection;
+import modeles.Livraison;
+import vue.Fenetre;
+
+public class EtatIntervertirLivraisons1 extends EtatDefault{
+	@Override
+	public void clicgauche(Controleur controleur, Fenetre fenetre, Point point, ListeDeCommandes listeDeCommandes)
+	{
+		System.out.println("test1");
+		controleur.plan.getAtPoint(point, controleur.getToleranceClic());
+		Intersection livraison1 = controleur.plan.getSelectedIntersection();
+		List<Livraison> Listelivraisons = controleur.tournee.getLivraisonsOrdonnees();
+		int posLivraison1 = -1;
+		for ( int i = 0; i < Listelivraisons.size() ; i++)
+		{
+			Livraison livraison = Listelivraisons.get(i);
+			if ( livraison.getIntersection().equals( livraison1 ) )
+			{
+				 posLivraison1 = i;
+			}
+		}
+		if (posLivraison1 != -1)
+		{
+			controleur.setEtatCourant( controleur.etatIntervertirLivraisons2);
+			controleur.etatIntervertirLivraisons2.posLivraison1 = posLivraison1;
+			fenetre.setBarreChargement("Premier point selectionne \n Cliquez sur un point de livraison du plan ou de la liste");
+		}
+		
+		
+	}
+	
+	@Override
+	public void modificationDansLaListe(Controleur controleur, ListeDeCommandes listeDeCommandes) {
+		int index = controleur.fenetre.getVueTextuelle().getListPanel().getCurrentSelection();
+		List<Livraison> Listelivraisons = controleur.tournee.getLivraisonsOrdonnees();
+		if(index > 0 && index <= Listelivraisons.size())
+		{
+			Livraison livraison = Listelivraisons.get(index-1);
+			controleur.plan.getLivraison(livraison);
+			
+			controleur.setEtatCourant( controleur.etatIntervertirLivraisons2);
+			controleur.etatIntervertirLivraisons2.posLivraison1 = index;
+		}
+	}
+	
+	@Override
+	public void undo(Controleur controleur, ListeDeCommandes listeDeCommandes, Fenetre fenetre)
+	{
+		controleur.setEtatCourant( controleur.etatModificationTournee);
+		fenetre.setModeModificationTournee();
+	}
+	
+	@Override
+	public void mouseDrag(Controleur controleur, Point delta)
+	{
+		controleur.fenetre.getVueGraphique().getMapPanel().drag(delta);
+	}
+	
+	@Override
+	public void mouseWheel(Controleur controleur, int steps, Point point)
+	{
+		controleur.fenetre.getVueGraphique().getMapPanel().zoom(steps, point);
+	}
+
+}
